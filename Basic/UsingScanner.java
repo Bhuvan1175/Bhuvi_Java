0326d4a0c196202108578d69f848765144137e94
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 13:42:54 +0530

    UsingScanner.java

---
+++ Basic/UsingScanner.java
@@ -0,0 +1,14 @@
+//WAP To Print The Addition Of Two Number Entered By User!!
+
+import java.lang.*;
+import java.util.*;
+class Readingfromkeyword{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The 2 number :");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("Sum Is "+c);
+	}
+}
