96d71ab4c4fa90f3da22ce19b6c5a9820d2e337e
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 14:17:55 +0530

    Update 05_Swaping(Using Third varible).java

--- Basic/05_Swaping(Using Third varible).java
+++ Basic/05_Swaping(Using Third varible).java
@@ -5,15 +5,15 @@ class Swaping {
     int x=67;
     int y=89;
     int Temp;
-    System.out.println("Before Swaping : ");
-    System.out.println("Before Swaping x : "+ x);
-    System.out.println("Before Swaping y : "+ y);
+    System.out.println("Before Swaping ");
+    System.out.println("x : "+ x);
+    System.out.println("y : "+ y);
     Temp=x;
     x=y;
     y=Temp;
-    System.out.println("After Swaping : ");
-    System.out.println("After Swaping x : "+ x);
-    System.out.println("After Swaping y : "+ y);
+    System.out.println("After Swaping ");
+    System.out.println("x : "+ x);
+    System.out.println("y : "+ y);
   }
 }
       
