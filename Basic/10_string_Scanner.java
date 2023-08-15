d979d78a1f01f155d565efcbbe2f69c8696f0fd5
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 16:54:44 +0530

    Create 10_string_Scanner.java

---
+++ Basic/10_string_Scanner.java
@@ -0,0 +1,14 @@
+//Write A Program To print Name entered By An User !!
+import java.lang.*;
+import java.util.*;
+class InputString{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    String Name;
+    System.out.println("May I Know Your Name ?");
+    Name = sc.nextLine();
+    System.out.println("welcome Mr./ Ms. "+ Name );
+  }
+}
+    
+    
