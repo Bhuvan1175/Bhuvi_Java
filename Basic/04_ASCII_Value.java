b1a25c4412783916b02c3c4f6fce9ccc08de7330
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 14:00:52 +0530

    Create 04_ASCII_Value.java

---
+++ Basic/04_ASCII_Value.java
@@ -0,0 +1,10 @@
+//Write A Program To Print A ASCII value Of A Given Special Letter!!
+import java.lang.*;
+class ASCII{
+    public static void main(String args[]){
+        int value='U';
+        System.out.println("ASCII Value is "+value);
+    }
+}
+
+//Code By Bhuvi
