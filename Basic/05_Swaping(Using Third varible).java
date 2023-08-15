1150a7838fbf62523fe3a0b586ffea6c56dae68b
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 14:08:47 +0530

    Create 05_Swaping(Using Third varible).java

---
+++ Basic/05_Swaping(Using Third varible).java
@@ -0,0 +1,21 @@
+// Write A Program To Swap The Value With Using Third Variable !!
+import java.lang.*;
+class Swaping {
+  public static void main(String args[]){
+    int x=67;
+    int y=89;
+    int Temp;
+    System.out.println("Before Swaping : ");
+    System.out.println("Before Swaping x : "+ x);
+    System.out.println("Before Swaping y : "+ y);
+    Temp=x;
+    x=y;
+    y=Temp;
+    System.out.println("After Swaping : ");
+    System.out.println("After Swaping x : "+ x);
+    System.out.println("After Swaping y : "+ y);
+  }
+}
+      
+      
+    
