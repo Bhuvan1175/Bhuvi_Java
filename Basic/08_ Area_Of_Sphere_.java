064446fc7bef49ce125dc51921b90945ea6c3cbd
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 16:24:26 +0530

    Create 08_ Area_Of_Sphere_.java

---
+++ Basic/08_ Area_Of_Sphere_.java
@@ -0,0 +1,17 @@
+//Write A Program To Print The Area Of Sphere!!
+import java.lang.*;
+import java.util.*;
+class AOS{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    double Area_Of_Sphere;
+    double pi=3.14;
+    int r;
+    System.out.println("Enter The Radius Of Sphere :");
+    r=sc.nextInt();
+    Area_Of_Sphere = 4*pi*r*r;
+    System.out.println(" Area_Of_Sphere is "+  Area_Of_Sphere );
+  }
+}
+
+//Code By Bhuvi
