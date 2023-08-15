271a8bed4dbc5e1eabc6b0b17ab71763d2bd073c
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-15 13:54:00 +0530

    Multiplication.java

---
+++ Basic/Multiplication.java
@@ -0,0 +1,16 @@
+// Write A Program To Print The Multiplication of Two Different Data Type Number Entered By An User !!
+import java.lang.*;
+import java.util.*;
+class Multiplication{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter The Integer Number  :");
+    int a = sc.nextInt();
+    System.out.println("Enter The Floating Number  :");
+    float b = sc.nextFloat();
+    float c=a*b;
+    System.out.println("Multiplication Is "+c);
+  }
+}
+    
+    
