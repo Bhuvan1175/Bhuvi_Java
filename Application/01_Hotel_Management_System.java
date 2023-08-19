3c03b279fc9591146fa884769e735f67bb82f7df
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2023-08-19 22:12:32 +0530

    Create 01_Hotel_Management_System.java

---
+++ Application/01_Hotel_Management_System.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Hotel{
+  public static void main(String arg[]){
+    System.out.println("----------------------------------------------------------------------------"+
+                       "\n-------------------------Welcome To Hotel Crispy!!--------------------------"+
+                       "\n----------------------------------------------------------------------------");
+    System.out.println("                                                                              ");
+    System.out.println("####################### MENU CARD OF CRISPY HOTEL ##########################");
+    System.out.println(" Item number | Item name \t\t\t\t\t | Price\t\t");
+    System.out.println(" 1. \t\t| Howzatt Biryani Combo  \t\t | 725 /-  ");
+    System.out.println(" 2. \t\t| Super 6s Bucket meal   \t\t | 978 /-  ");
+    System.out.println(" 3. \t\t| Super 4s Bucket meal   \t\t | 608 /-  ");
+    System.out.println(" 4. \t\t| 5pc Smoky Red Chicken  \t\t | 524 /-  ");
+    System.out.println(" 5. \t\t| Classic Biryani Combo  \t\t | 777 /-  ");
+    System.out.println(" 6. \t\t| Tandoori Zinger Burger \t\t | 209 /-  ");
+    System.out.println(" 7. \t\t| Cold Drink             \t\t | 040 /-  ");
+    System.out.println(" 8. \t\t| Classic Zinger meal    \t\t | 362 /-  ");
+    
